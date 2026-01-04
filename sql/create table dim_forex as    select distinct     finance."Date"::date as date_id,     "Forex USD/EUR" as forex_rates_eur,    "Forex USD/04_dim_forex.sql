1c674256e20e9652c64f53cff645cf34217ea50d
rishikaydave-data <rishikaydave@gmail.com>
2026-01-04 11:57:59 +0530

    Update 04_dim_forex.sql

--- sql/create table dim_forex as    select distinct     finance."Date"::date as date_id,     "Forex USD/EUR" as forex_rates_eur,    "Forex USD/04_dim_forex.sql
+++ sql/create table dim_forex as    select distinct     finance."Date"::date as date_id,     "Forex USD/EUR" as forex_rates_eur,    "Forex USD/04_dim_forex.sql
@@ -1,7 +1,1 @@
-create table dim_forex as
-   select distinct 
-   finance."Date"::date as date_id, 
-   "Forex USD/EUR" as forex_rates_eur,
-   "Forex USD/JPY" as forex_rates_jpy
-   
-   from finance
+
